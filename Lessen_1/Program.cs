aaa5ef03c417ad94802694a9035915a8f678cd38
Kayabo <griha35654@478@gmail.com>
2022-11-13 19:00:47 +0300

    порешал задачку

--- Lessen_1/Program.cs
+++ Lessen_1/Program.cs
@@ -1,12 +1,57 @@
-﻿// Написать программу которая принимает число и выдает его квадрат
-Console.WriteLine("Введите число :");
-int number = int.Parse(Console.ReadLine()!);
+﻿
 
-// int sqr = number * number;
+// Console.WriteLine("Введите два целых числа");
+// int a = int.Parse(Console.ReadLine()!);
+// int b = int.Parse(Console.ReadLine()!);
+// if(a < b)
+// {
+//     Console.WriteLine($"Число {b}, больше числа {a}");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {a}, больше числа {b}");
+// }
 
-// Console.WriteLine($"Квадрат числа {number } = {sqr}");
+// Console.WriteLine("Введите три целых числа");
+// int a = int.Parse(Console.ReadLine()!);
+// int b = int.Parse(Console.ReadLine()!);
+// int c = int.Parse(Console.ReadLine()!);
+// if( a > b && a > c)
+// {
+//     Console.WriteLine($"Самое большое число из трех это {a}");
+// }
 
 
-// Вариант 2
-int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
-Console.WriteLine($"Квадрат числа {number } = {sqr1}");
+
+
+// if( b > a && b > c)
+// {
+//     Console.WriteLine($"Самое большое число из трех это {b}");
+// }
+// else
+// {
+//     Console.WriteLine($"Самое большое число из трех это {c}");
+// }
+
+
+
+// Console.WriteLine("Целое число");
+// int a = int.Parse(Console.ReadLine()!);
+// if (a % 2 == 0)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+
+// Console.WriteLine("Целое число");
+// int a = int.Parse(Console.ReadLine()!);
+// int counter = 0;
+// while(counter + 1 < a)
+// {
+//     counter += 2;
+//     Console.WriteLine(counter);
+// }
